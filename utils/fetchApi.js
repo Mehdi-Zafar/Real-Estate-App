afded551830219a182fd104348bead21e9bd9de7
Mehdi-Zafar <mehdizaffar0@gmail.com>
2022-12-12 20:39:06 +0500

    7th commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url)=>{
     const {data} = await axios.get((url),{
         headers: {
-            'X-RapidAPI-Key': 'dea7d50e4cmsh3988ca60d841694p1daa4cjsndca94800ad96',
+            'X-RapidAPI-Key': process.env.API_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     });
